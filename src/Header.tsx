021560b5a6aa932431c5f8bb0d67bb317f26bdea
Tristan K <tristanmkernan@gmail.com>
2019-11-08 17:48:41 -0500

    fix: header display on mobile

--- src/Header.tsx
+++ src/Header.tsx
@@ -13,51 +13,32 @@ const Header = ({ wsStatus }: Props) => (
             <a className="navbar-item" href="/">
                 Is the Bathroom Available?
             </a>
+            <div className="navbar-item connected-status">
+                {
+                    wsStatus === ConnectionStatus.CONNECTED && (
+                        <>
+                            <span className="connected-status-message is-hidden-mobile">
+                                Connected
+                            </span>
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faWifi}/>
+                            </span>
+                        </>
+                    )
+                }
 
-            <a
-                role="button"
-                className="navbar-burger burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-            >
-                <span aria-hidden="true"/>
-                <span aria-hidden="true"/>
-                <span aria-hidden="true"/>
-            </a>
-        </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-end">
-                <div className="navbar-item">
-                    <div className="connected-status">
-                        {
-                            wsStatus === ConnectionStatus.CONNECTED && (
-                                <>
-                                <span className="connected-status-message">
-                                    Connected
-                                </span>
-                                    <span className="icon">
-                                    <FontAwesomeIcon icon={faWifi}/>
-                                </span>
-                                </>
-                            )
-                        }
-
-                        {
-                            wsStatus === ConnectionStatus.DISCONNECTED && (
-                                <>
-                                <span className="connected-status-message">
-                                    Disconnected
-                                </span>
-                                    <span className="icon">
-                                    <FontAwesomeIcon icon={faExclamationTriangle}/>
-                                </span>
-                                </>
-                            )
-                        }
-                    </div>
-                </div>
+                {
+                    wsStatus === ConnectionStatus.DISCONNECTED && (
+                        <>
+                            <span className="connected-status-message is-hidden-mobile">
+                                Disconnected
+                            </span>
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faExclamationTriangle}/>
+                            </span>
+                        </>
+                    )
+                }
             </div>
         </div>
     </nav>
